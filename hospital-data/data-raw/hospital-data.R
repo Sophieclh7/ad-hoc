a3b2a47cf78dc3f2377c08a523f0b8b756947b6b
Sophie Hammond <sophieclhammond@gmail.com>
2024-07-24 16:47:23 +0100

    Created trusts and wards dataset

---
+++ hospital-data/data-raw/hospital-data.R
@@ -0,0 +1,119 @@
+# ---- Load packages ----
+library(readxl)
+library(tidyverse)
+library(httr)
+library(readr)
+library(utils)
+library(dplyr)
+
+# ---- Load ward names dataset ----
+#Scrape URL
+#Source: https://www.england.nhs.uk/publication/care-hours-per-patient-day-chppd-data/
+#Ward name dataset
+url <- "https://www.england.nhs.uk/wp-content/uploads/2021/02/nhs-england-ward-level-chppd-feb-2024.xlsx"
+
+#Download and read URL as temp file
+temp_file <- tempfile(fileext = ".xlsx")
+download.file(url, temp_file, mode = "wb")
+ward_data <- read_excel(temp_file)
+
+# ---- Load NHS hospitals dataset ----
+contact_data <- read_excel("hospital-data/data/nhs_hospital_info.xlsx")
+
+#Organise ODS code column in ascending order
+contact_data <- contact_data |>
+  arrange(`ODS Code`)
+
+# ---- Load geographr package ----
+library(remote)
+remotes::install_github("humaniverse/geographr")
+library(geographr)
+
+# ---- Load ltla and trusts lookup ----
+data("lookup_nhs_trusts22_ltla21", package = "geographr")
+
+# ---- Load postcode and ltla file ----
+#Source: https://www.arcgis.com/sharing/rest/content/items/bc8f6d1f6ee64111b6a59b22c6605f3b/data
+# Define the URL and the destination file path
+url <- "https://www.arcgis.com/sharing/rest/content/items/bc8f6d1f6ee64111b6a59b22c6605f3b/data"
+destfile <- "dataset.zip"
+
+#Download the file
+download.file(url, destfile)
+
+#Unzip the downloaded file
+unzip(destfile, exdir = "dataset")
+
+#Load dataset csv file
+csv_file_path <- "dataset/PCD_OA21_LSOA21_MSOA21_LTLA22_UTLA22_CAUTH22_NOV23_UK_LU_v2.csv"
+postcodes_data <- read.csv(csv_file_path)
+
+#Rename postcode column
+postcode_data <- postcodes_data |>
+  rename(
+    "Postcode" = `pcds`,
+    "ltla21_code" = ltla22cd)
+
+# ---- Merge postcode data to NHS data by postcode ----
+postcode_join <- left_join(contact_data, postcode_data, by = "Postcode")
+
+#Get rid of duplicates
+regions_df <- lookup_ltla21_brc[!duplicated(lookup_ltla21_brc$ltla21_code), ]
+
+# ---- Merge postcode join to BRC data using ltla code ----
+postcode_regions_df <- left_join(postcode_join, regions_df, by = "ltla21_code", relationship = "many-to-many")
+
+# ---- Load site code and postcode file
+#Source: https://digital.nhs.uk/services/organisation-data-service/export-data-files/csv-downloads/other-nhs-organisations
+#Define the URL and temporary file paths
+url <- "https://files.digital.nhs.uk/assets/ods/current/etrust.zip"
+temp_zip <- tempfile(fileext = ".zip")
+temp_dir <- tempdir()
+
+# Download the ZIP file
+GET(url, write_disk(temp_zip))
+
+# Unzip file to temporary directory
+unzip(temp_zip, exdir = temp_dir)
+
+# List files in the directory to identify the CSV file
+csv_files <- list.files(temp_dir, pattern = "\\.csv$", full.names = TRUE)
+
+# Check if CSV file found
+if (length(csv_files) == 0) {
+  stop("No CSV files found in the ZIP archive.")
+}
+
+# Read the CSV file
+site_data <- read.csv(csv_files[1])
+
+# ---- Map postcodes to ward names ----
+#Rename site code and select only relevant columns before joining
+site_data <- site_data |>
+  rename("Site Code" = `A0A1E`) |>
+  dplyr::select("Site Code", "AL10.8HR")
+ward_data <- ward_data |>
+  dplyr::select("Organisation Name", "Site Name", "Site Code", "Ward Name")
+
+#Join data
+site_join <- left_join(ward_data, site_data, by = "Site Code")
+
+# ---- Join ward names to hospital dataset ----
+#Rename column before join
+site_join <- site_join |>
+  rename(
+    "Postcode" = `AL10.8HR`)
+
+#Join datasets
+trust_data <- left_join(site_join, merged_df, by = "Postcode", relationship = "many-to-many")
+
+#Keep only required columns
+trust_data <- trust_data |>
+  dplyr::select("Organisation Name.y", "ODS Code", "brc_area", "Address1", "Address2", "Address3", "City", "County", "Postcode", "Telephone", "Email", "Website", "Ward Name", "Site Name")
+
+#To find hospitals with missing data
+#Some hospitals are in ward names dataset but not in hospital information dataset sent by NHS
+na_df <- trust_data |>
+  filter(is.na(brc_area))
+
+write_csv(trust_data, "hospital-data/data/trust-ward.csv")
